e5e68d6037d10cca89a55e284915097e795dc778
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-12-22 15:58:01 -0800

    Added an example that uses a web worker.

---
+++ example_webworker/main.js
@@ -0,0 +1,80 @@
+// Copyright (c) 2015 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// This software is licensed under a MIT License
+// https://github.com/workhorsy/uncompress.js
+
+var entryList = null;
+
+function toFriendlySize(size) {
+	if (size >= 1024000000) {
+		return (size / 1024000000).toFixed(2) + ' GB';
+	} else if (size >= 1024000) {
+		return (size / 1024000).toFixed(2) + ' MB';
+	} else if (size >= 1024) {
+		return (size / 1024).toFixed(2) + ' KB';
+	} else if (size >= 1) {
+		return (size / 1).toFixed(2) + ' B';
+	} else if (size === 0) {
+		return '0 B';
+	}
+
+	return '?';
+}
+
+window.onload = function() {
+	entryList = document.getElementById('entryList');
+
+	// Create the web worker
+	var worker = new Worker('worker.js');
+	worker.onmessage = function(e) {
+		switch (e.data.action) {
+			case 'uncompress_each':
+				var file_name = e.data.file_name;
+				var url = e.data.url;
+				var length = e.data.length;
+
+				// Add a BR to the document
+				entryList.appendChild(document.createElement('br'));
+
+				// Add a link to the Object URL
+				var a = document.createElement('a');
+				a.href = url;
+				a.innerHTML = file_name + ' (' + toFriendlySize(length) + ')';
+				entryList.appendChild(a);
+				break;
+			case 'invalid_file':
+				entryList.innerHTML = e.data.error;
+				break;
+		}
+	};
+
+	// Try loading each archive that is selected
+	document.getElementById('fileInput').onchange = function() {
+		// Just return if there is no file selected
+		var file_input = document.getElementById('fileInput');
+		if (file_input.files.length === 0) {
+			entryList.innerHTML = 'No file selected';
+			return;
+		}
+		entryList.innerHTML = '';
+
+		// Get the file's info
+		var file = file_input.files[0];
+		var blob = file.slice();
+		var file_name = file.name;
+
+		// Convert the blob into an array buffer
+		var reader = new FileReader();
+		reader.onload = function(evt) {
+			var array_buffer = reader.result;
+
+			// Send the file name and array buffer to the web worker
+			var message = {
+				action: 'uncompress_start',
+				file_name: file_name,
+				array_buffer: array_buffer
+			};
+			worker.postMessage(message);
+		};
+		reader.readAsArrayBuffer(blob);
+	};
+};
---
+++ example_webworker/worker.js
@@ -0,0 +1,102 @@
+// Copyright (c) 2015 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// This software is licensed under a MIT License
+// https://github.com/workhorsy/uncompress.js
+"use strict";
+
+importScripts("../js/libunrar.js");
+importScripts("../js/jszip.js");
+importScripts("../js/libuntar.js");
+importScripts("../js/uncompress.js");
+
+
+// FIXME: This function is super inefficient
+function isValidImageType(file_name) {
+	file_name = file_name.toLowerCase();
+	return file_name.endsWith('.jpeg') ||
+			file_name.endsWith('.jpg') ||
+			file_name.endsWith('.png') ||
+			file_name.endsWith('.bmp') ||
+			file_name.endsWith('.gif');
+}
+
+// FIXME: This function is super inefficient
+function getFileMimeType(file_name) {
+	file_name = file_name.toLowerCase();
+	if (file_name.endsWith('.jpeg') || file_name.endsWith('.jpg')) {
+		return 'image/jpeg';
+	} else if (file_name.endsWith('.png')) {
+		return 'image/png';
+	} else if (file_name.endsWith('.bmp')) {
+		return 'image/bmp';
+	} else if (file_name.endsWith('.gif')) {
+		return 'image/gif';
+	} else {
+		// Uses jpeg as default mime type
+		return 'image/jpeg';
+	}
+}
+
+function onUncompress(archive) {
+	// Get only the entries that are images
+	var entries = [];
+	archive.entries.forEach(function(entry) {
+		if (isValidImageType(entry.name)) {
+			entries.push(entry);
+		}
+	});
+
+	// Uncompress each entry and send it to the client
+	var onEach = function(i) {
+		if (i >= entries.length) {
+			return;
+		}
+
+		var entry = entries[i];
+		entry.readData(function(data) {
+			if (entry.is_file) {
+				var blob = new Blob([data], {type: getFileMimeType(entry.name)});
+				var url = URL.createObjectURL(blob);
+
+				var message = {
+					action: 'uncompress_each',
+					file_name: entry.name,
+					url: url,
+					index: i,
+					length: data.byteLength
+				};
+				self.postMessage(message);
+			}
+
+			// FIXME: Change readData so it uses setTimeout internally with rar
+			setTimeout(function() {
+				onEach(i + 1);
+			}, 0);
+		});
+	};
+	onEach(0);
+}
+
+self.addEventListener('message', function(e) {
+	switch (e.data.action) {
+		case 'uncompress_start':
+			// Get the file data
+			var array_buffer = e.data.array_buffer;
+			var file_name = e.data.file_name;
+
+			// Open the file as an archive
+			var archive = archiveOpen(file_name, array_buffer);
+			if (archive) {
+				console.info('Uncompressing ' + archive.archive_type + ' ...');
+				onUncompress(archive);
+			// Otherwise show an error
+			} else {
+				var error = 'Invalid file: "' + file_name + '"';
+				var message = {
+					action: 'invalid_file',
+					error: error
+				};
+				self.postMessage(message);
+			}
+			break;
+	}
+}, false);
