e412ae2889568a26166ac41d6a199235a2ede0ac
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-07-21 14:44:40 -0700

    Cleanup of scope in examples.

--- examples/trivial/main.js
+++ examples/trivial/main.js
@@ -2,42 +2,41 @@
 // This software is licensed under a MIT License
 // https://github.com/workhorsy/uncompress.js
 
+var fileInput = document.getElementById('fileInput');
+var filePassword = document.getElementById('filePassword');
+var entryList = document.getElementById('entryList');
+var errorList = document.getElementById('errorList');
+
+function onArchiveLoaded(archive) {
+	var is_error = false;
+	archive.entries.forEach(function(entry) {
+		if (! entry.is_file) return;
+		if (is_error) return false;
+
+		entry.readData(function(data, err) {
+			if (err) {
+				is_error = true;
+				errorList.innerHTML = err;
+				entryList.innerHTML = '';
+				return;
+			}
+
+			entryList.innerHTML +=
+			'<b>Name:</b> ' + entry.name + '<br />' +
+			'<b>Compressed Size:</b> ' + entry.size_compressed + '<br />' +
+			'<b>Uncompressed Size:</b> ' + entry.size_uncompressed + '<br />' +
+			'<b>Is File:</b> ' + entry.is_file + '<br />';
+
+			var url = URL.createObjectURL(new Blob([data]));
+			entryList.innerHTML += '<a href="' + url + '">download</a>' + '<br />';
+
+			entryList.innerHTML += '<hr />';
+		});
+	});
+}
 
 // Load all the archive formats
 loadArchiveFormats(['rar', 'zip', 'tar'], function() {
-	var fileInput = document.getElementById('fileInput');
-	var filePassword = document.getElementById('filePassword');
-	var entryList = document.getElementById('entryList');
-	var errorList = document.getElementById('errorList');
-
-	function onArchiveLoaded(archive) {
-		var is_error = false;
-		archive.entries.forEach(function(entry) {
-			if (! entry.is_file) return;
-			if (is_error) return false;
-
-			entry.readData(function(data, err) {
-				if (err) {
-					is_error = true;
-					errorList.innerHTML = err;
-					entryList.innerHTML = '';
-					return;
-				}
-
-				entryList.innerHTML +=
-				'<b>Name:</b> ' + entry.name + '<br />' +
-				'<b>Compressed Size:</b> ' + entry.size_compressed + '<br />' +
-				'<b>Uncompressed Size:</b> ' + entry.size_uncompressed + '<br />' +
-				'<b>Is File:</b> ' + entry.is_file + '<br />';
-
-				var url = URL.createObjectURL(new Blob([data]));
-				entryList.innerHTML += '<a href="' + url + '">download</a>' + '<br />';
-
-				entryList.innerHTML += '<hr />';
-			});
-		});
-	}
-
 	fileInput.onchange = function() {
 		// Just return if there is no file selected
 		if (fileInput.files.length === 0) {
